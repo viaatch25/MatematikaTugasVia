6185eeaaf00be9cd82368de64c66589a1a6b59ba
viaatch25 <bunnswiftiesbump@gmail.com>
2024-11-24 09:04:41 +0700

    summary

---
+++ src/matematika/MatematikaCanggihBeraksi.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package matematika;
+
+/**
+ *
+ * @author ThinkAgain
+ */
+public class MatematikaCanggihBeraksi {
+public static void main(String[] args) {
+MatematikaCanggih Mtk = new MatematikaCanggih();
+
+Mtk.setpertambahan(27,28);
+System.out.println("Hasil Tambah="+Mtk.getpertambahan());
+Mtk.setperkalian(15,9);
+System.out.println("Hasil Kali="+Mtk.getperkalian());
+Mtk.setpembagian(14,7);
+System.out.println("Hasil Bagi="+Mtk.getpembagian());
+Mtk.setmodulus(9,17);
+System.out.println("Hasil Modulus="+Mtk.getmodulus());
+}
+}
